7bfea4ee0ef47574061010f3b50d93dc71543400
Ngữ Quang Trương <zquangu112z@gmail.com>
2018-03-07 17:34:58 +0700

    refactor: clean up

--- memPerProvince.R
+++ memPerProvince.R
@@ -1,14 +1,18 @@
 library("tmap")
 library("tmaptools")
-library("readxl")
-data <- read_xlsx("abxmem.xlsx")
+library("openxlsx")
+data <- read.xlsx("abxmem.xlsx")
 head(data)
 vnshapefile <- "Provinces.shp"
 if (file.exists(vnshapefile) == TRUE){
   vngeo <- read_shape(file = vnshapefile, as.sf = TRUE)
-  # qtm(vngeo)
   vnmap <- append_data(vngeo, data, key.shp = "VARNAME_2", key.data ="province")
   qtm(vnmap, "mem")
 }else{
   print("Data file does not exist")
 }
+
+
+# for fun
+library("beepr")
+beep("random")
